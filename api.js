9f197fbdaea18fa14d2fad414ae8ddf8723a3472
Jaydeep Karpe <karpepatil6@gmail.com>
2025-01-02 18:34:02 +0530

    Initial commit

---
+++ api.js
@@ -0,0 +1,16 @@
+// var myHeaders = new Headers();
+// myHeaders.append("apikey", "hb8pW1yeiOH842waFY39707UNmAWxNNe");
+
+// var requestOptions = {
+//   method: "GET",
+//   redirect: "follow",
+//   headers: myHeaders,
+// };
+
+// fetch(
+//   "https://api.apilayer.com/exchangerates_data/latest?symbols=&base=",
+//   requestOptions
+// )
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));
